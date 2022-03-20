12a6e4cd3fa7c3592cff0df89388bd52e874b5c1
dominik.kalamus <dominik.kalamus@gmail.com>
2022-03-20 21:08:02 +0100

    Override RankedGradeBook's CalculateStatistics method

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -30,5 +30,17 @@ namespace GradeBook.GradeBooks
                 return 'D';
             return 'F';
         }
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("You need 5 students at least with grades");
+            }
+            else
+            {
+                base.CalculateStatistics();
+            }
+        }
+
     }
 }
